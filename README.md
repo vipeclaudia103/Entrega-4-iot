a2a03b403337642f45f0488b56e299885bf04f8e
clau.v.perlado <vipeclaudia103@gmail.com>
2024-04-10 20:22:54 +0200

    Envio de datos cada segundo leyendo y cargando el csv

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Entrega-4-iot
 Repositorio para la entrega de la cuarta práctica en la asignatura de Desarrollo de aplicaciones IOT
+
+Miembros del equipo: Claudia Viñals
+
+# Explicación de los pasos seguidos
+
+## Instalar InfluxDB 2
+Instalar InfluxDB 2.0 como servicio:
+
+```bash
+# Ubuntu/Debian AMD64
+curl -O https://dl.influxdata.com/influxdb/releases/influxdb2_2.7.5-1_amd64.deb
+sudo dpkg -i influxdb2_2.7.5-1_amd64.deb
+
+```
+
+Iniciar el serrvicio
+
+```bash
+sudo service influxdb start
+```
+
+token de influx:
+```
+SXzum1org1t_mYU5dxdPB7o9sQHLqtv3govXNySAFcmWNuQhwT4_PaooIeKmX2g3F0bHdzApWNenWx6sG9tErA==
+```
+Para guardar la varariable den token en el entorno ejecutar:
+```bash
+export INFLUXDB_TOKEN=hiwt2t-GCL8w6qS9HZFZ6oDfwqYUdRscZ3uX8urlPb_CBvW298IEE19zaeJU4JEEmc4LBWTuT2aR_bcAiA9p0g==
+```
+
+## Instalar libreria de InfluxDB client
+Para enviar los datos a InfluxDB es necesario instalar la libreria de python correspondiente.
+
+```bash
+pip3 install influxdb-client
+```
+
+# Instrucciones de uso
+# Posibles vías de mejora
+# Problemas / Retos encontrados
+# Alternativas posibles
---
+++ envio.py
@@ -0,0 +1,49 @@
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+from datetime import datetime
+import csv
+import time
+
+
+# Configuración de conexión a InfluxDB
+token = "iH8lRT8epPlzBWGya3EkKwOCKEa2fSj9SAtTGY_ytNHwrye73hvakM98MDoe7He9sAYTO34jRzJgb8_QsvSVCg=="
+org = "CVP"
+bucket = "Molinos"
+url = "http://localhost:8086"
+
+
+client = InfluxDBClient(url=url, token=token,  org=org)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+# Función para leer el archivo CSV y cargar los datos en InfluxDB
+def load_csv_to_influxdb(csv_file):
+    with open(csv_file, 'r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)  # Saltar la fila de encabezado
+        for row in csv_reader:
+            # Obtener el timestamp actual en formato UNIX en milisegundos
+            timestamp = int(datetime.now().timestamp() * 1000)
+            lv_active_power = float(row[1])  # Potencia activa LV (kW)
+            wind_speed = float(row[2])  # Velocidad del viento (m/s)
+            theoretical_power_curve = float(row[3])  # Curva de potencia teórica (KWh)
+            wind_direction = float(row[4])  # Dirección del viento (°)
+            
+            # Crear un punto de datos
+            point = Point("turbina") \
+                .time(timestamp, 'ms') \
+                .field("lv_active_power", lv_active_power) \
+                .field("wind_speed", wind_speed) \
+                .field("theoretical_power_curve", theoretical_power_curve) \
+                .field("wind_direction", wind_direction)
+            time.sleep(1)
+            # Escribir el punto de datos en InfluxDB
+            write_api.write(bucket=bucket, org=org, record=point)
+
+# Ruta al archivo CSV
+csv_file_path = "Entrega-4-iot/T1.csv"
+
+# Cargar datos desde el archivo CSV a InfluxDB cada 10 minutos
+while True:
+    load_csv_to_influxdb(csv_file_path)
+    print("Datos cargados en InfluxDB")
+      # Esperar 10 minutos antes de cargar los datos nuevamente
